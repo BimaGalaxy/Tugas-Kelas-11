e3f67e606a0a21f8e9bd01573251b451e83ffe39
Bima Galaxy <bima68056@gmail.com>
2024-01-22 14:53:20 +0700

    Update Button.js

--- pacasila-button-formOsis/src/Components/Button.js
+++ pacasila-button-formOsis/src/Components/Button.js
@@ -2,15 +2,21 @@
 import React, {Component} from 'react';
 import { Button } from 'react-bootstrap';
 class CustomButton extends Component {
+    handleClick = (message) => {
+        alert(message);
+    };
     render() {
-
         const buttonStyle = {
             width: this.props.width,
             height: this.props.height,
         };
 
         return (
-            <Button variant={this.props.variant} style={buttonStyle}>
+            <Button
+                variant={this.props.variant}
+                style={buttonStyle}
+                onClick={() => this.handleClick(`Tombol ${this.props.variant} diklik!`)}
+            >
                 {this.props.children}
             </Button>
         );
